cb3c3d4174ca7b9c6d7346d51672e62e5a0808a0
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-11-09 17:58:13 +0300

    add links

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const Isotope = require('isotope-layout');
+const imagesLoaded = require('imagesloaded');
 
 const elem = document.querySelector('.grid');
-const iso = new Isotope( elem, {
-  // options
-  itemSelector: '.grid__item',
-  layoutMode: 'masonry',
-  masonry: {
-    columnWidth: '.grid__sizer',
-    gutter: 10
-  }
+
+imagesLoaded(elem, function (instance) {
+  console.log('all images are loaded');
+  const iso = new Isotope(elem, {
+    itemSelector: '.grid__item',
+    layoutMode: 'masonry',
+    masonry: {
+      columnWidth: '.grid__sizer',
+      gutter: 25
+    }
+  });
 });
