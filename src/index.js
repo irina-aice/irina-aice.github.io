0e4b2fd5d20533222c184b7c6dd5adc712127f39
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-11-11 12:29:08 +0300

    add filter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const imagesLoaded = require('imagesloaded');
 const elem = document.querySelector('.grid');
 
 imagesLoaded(elem, function (instance) {
-  console.log('all images are loaded');
   const iso = new Isotope(elem, {
     itemSelector: '.grid__item',
     layoutMode: 'masonry',
@@ -13,4 +12,31 @@ imagesLoaded(elem, function (instance) {
       gutter: 25
     }
   });
+
+  const filtersElem = document.querySelector('.js-filters-button-group');
+  filtersElem.addEventListener('click', (evt) => {
+    if (!evt.target.closest('button')) {
+      return false;
+    }
+
+    let filterValue = evt.target.getAttribute('data-filter');
+    iso.arrange({filter: filterValue});
+  });
+
+  const buttonGroups = document.querySelectorAll('.js-button-group');
+  for (let i = 0; i < buttonGroups.length; i++) {
+    let buttonGroup = buttonGroups[i];
+    radioButtonGroup(buttonGroup);
+  }
+
+  function radioButtonGroup(buttonGroup) {
+    buttonGroup.addEventListener('click', function (evt) {
+      if (!evt.target.closest('button')) {
+        return false;
+      }
+
+      buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+      evt.target.classList.add('is-checked');
+    });
+  }
 });
